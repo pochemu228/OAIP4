3a6592d560ff42c256f257157251d3e8a6026511
pochemu228 <arinasmirnova096@gmail.com>
2025-03-04 08:37:46 +0900

    ex3.py

--- ex3.py
+++ ex3.py
@@ -1,10 +1,10 @@
 def func_table(f, x_max, y_max):
-
     for y in range(y_max + 1):
         table = []
         for x in range(x_max + 1):
-            table.append(str(eval(f)))
 
+            value = eval(f, {'x': x, 'y': y})
+            table.append(str(value))
         print("\t".join(table))
 
-func_table("x ** 2 + y", 5, 8)
+func_table("x ** 2 + y", 3, 5)
